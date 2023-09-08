3e901dc87542cfe7ccdae17395aec8858eceedba
Uzair Saleem <uzaircreative78@gmail.com>
2023-09-08 17:27:53 +0500

    two files added

---
+++ app.js
@@ -0,0 +1,97 @@
+// Chapter No 1 (Alert)
+// Answer No 1
+alert('Uzair')
+alert('Saleem')
+alert('uzaircreative78@gmail.com')
+alert('03242277683')
+alert('johnsmith30397')
+// Answer No 2
+alert('You are learning JavaScript')
+// Answer No 3
+alert('Hey I hope you are fine')
+
+// Chapter No 2 (Variables for string)
+// Answer No 1
+var firstTime
+// Answer No 2
+var my_Varibale
+my_Varibale = "Hello World"
+document.write(my_Varibale)
+// Answer No 3
+var teamName = "Uzitech"
+alert(teamName)
+// Answer No 4
+var bestMan = "John";
+bestMan = "Smith";
+
+// Chapter No 3 (Variables for numbers)
+// Answer No 1
+var caseQty
+// Answer No 2
+caseQty = "144"
+// Answer No 3
+var num = 9;
+// Answer No 4
+var num2 = 5;
+console.log(num + num2)
+// Answer No 5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal)
+// Answer No 6
+var num = 2;
+var Answer = num + 3;
+console.log(Answer)
+
+// Chapter No 4 (Variable names Legal and Illegal)
+// Answer No 1
+var productCost = 3.45;
+// Answer No 2
+var NameOfBand;
+// Answer No 3
+var myNumber = 42;
+// Answer No 4
+var ahmedSaleem;
+// Answer No 5
+// legal Name
+var camelCaseExample;
+var myVariable;
+var _privateVariable;
+var $specialVariable;
+var anotherVariable123;
+
+// Illegal Name
+
+// #invalidVariable;
+// my-variable;
+// my variable;
+// my*variable;
+
+// Chapter No 5 
+// Answer No 1
+// "modulo operator," and its symbol is %
+// Answer No 2
+var num = 20 % 6;
+console.log(num)
+// Answer No 3
+var largeNum = 1000;
+var totalNum = largeNum * 2000;
+console.log (totalNum)
+// Answer No 4
+var subNum = 45;
+var totalSubNum = subNum - 20;
+console.log (totalSubNum)
+// Answer No 5
+var resultRemainder = 15 % 4;
+console.log(resultRemainder)
+// Answer No 6
+var multiplication = 22
+var multiplication1 = 32
+alert(multiplication * multiplication1)
+
+
+// **********************THE END****************************
+
+
+
